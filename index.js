85a789b7325594ba707967ae2aa3f8f25243261a
arthurscutari <32248349+arthurscutari@users.noreply.github.com>
2023-06-07 21:23:48 -0300

    Migrando para o SCSS
    Arquivos base do SCSS.

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const divImgProjetos = document.querySelectorAll(".div-img-card");
 const divDescricaoProjetos = document.querySelectorAll(".div-descricao-projeto");
 const divHabilidadeProjetos = document.querySelectorAll(".div-icones-habilidades-card");
 const divProjetos = document.querySelectorAll(".div-projeto-1");
+const sectionProjetos = document.querySelector(".section-projetos");
+
 
 
 divImgProjetos.forEach((divImgProjeto, index) => {
   divImgProjeto.addEventListener('click', () => {
+    sectionProjetos.classList.add("aumentarSectionProjetos");
     divDescricaoProjetos[index].classList.toggle("aumentarDivDescricao");
     divHabilidadeProjetos[index].classList.toggle("aumentarDivHabilidade");
     divProjetos[index].classList.toggle("aumentarDivProjeto");
+
   });
 });
